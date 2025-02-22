9dd35b1981298ae6491931fd157626bb9c47ae4a
Arafat <arafatsharif7890@gmail.com>
2025-02-23 00:54:35 +0600

    requests

--- mo/Walrus Operator/Walrus Operator .py
+++ mo/Walrus Operator/Walrus Operator .py
@@ -1,10 +1,2 @@
-"""foods = list()
-while True:
-    food = input("What food do you like?: ")
-    if food == "Quit":
-        break
-    foods.append(food)"""
-    
-foods = list
-while (food := input("What food you like?")) != "quit":
-    foods.append(found_terminator())
+
+        
