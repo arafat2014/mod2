f5dbba16f7bdbb152e4976112fa60587b81f19d5
Arafat <arafatsharif7890@gmail.com>
2025-02-23 22:55:47 +0600

    requests

--- mo/main/main.py
+++ mo/main/main.py
@@ -1,3 +1,11 @@
 import requests 
-response= requests.get("https://www.youtube.com/")
-print(response.text)
+from bs4 import BeautifulSoup
+
+url = "https://www.youtube.com/"
+
+r = requests.get(url)
+
+soup = BeautifulSoup(r.text, "html.parser")
+
+print(soup.prettify())
+
